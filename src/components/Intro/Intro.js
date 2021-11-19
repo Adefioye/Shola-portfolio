da3799442de3b2ca9162d1e8e7ee13b6528b8888
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-11-19 03:08:25 +0100

    added portfolio images

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { ArrowForwardIos } from "@material-ui/icons";
+import { init } from "ityped";
 import "./Intro.css";
 
 function Intro() {
+  const ref = useRef();
+
+  useEffect(() => {
+    init(ref.current, {
+      showCursor: false,
+      strings: [
+        "Program Coordinator",
+        "Humanitarian Aid Worker",
+        "Content Writer",
+      ],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
-      <h1>Intro</h1>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="./assets/profile-pic.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h3>Hi awesome one! I'm</h3>
+        <h1>Adeshola Adeniji</h1>
+        <h3 ref={ref} className="job-title"></h3>
+        <a href="#portfolio">
+          <ArrowForwardIos className="arrow-down" />
+        </a>
+      </div>
     </div>
   );
 }
